0a494a586ae9961ca63cc7e19dfb91db6a6efe02
Mark Hahn <mark@hahnca.com>
2025-05-29 08:36:20 -0700

    copied marks, gutter, and sidebar from sticky bookmarks

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,11 @@
 import * as vscode from 'vscode';
 import * as cmds   from './commands';
+import * as gutt   from './gutter';
 import * as parse  from './parse';
 
 export function activate(context: vscode.ExtensionContext) {
+
+  gutt.activate(context);
 
 	const toggle = vscode.commands.registerCommand(
                 'vscode-function-marks.toggle', () => {
