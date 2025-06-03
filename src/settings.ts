2e90bcdb9207f6e469c23c098fc5303198aeb841
Mark Hahn <mark@hahnca.com>
2025-06-03 00:22:28 -0700

    added includeFiles to settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import * as mrks  from './marks';
+import {minimatch} from 'minimatch';
 import * as utils  from './utils';
 const {log} = utils.getLog('sett');
 
@@ -11,19 +11,25 @@ interface FunctionMarksSettings {
 export let settings: FunctionMarksSettings = {
   fileWrap:            true,
   includeSubFunctions: false,
-}; 
+};
 
 export function activate() {
   settings = getFunctionMarksSettings();
-  // log('Settings activated:', settings);
 }
 
-function mm(val: number, max: number, min: number = 0): number {
-  return Math.max(min, Math.min(max, val));
-}
+// function mm(val: number, max: number, min: number = 0): number {
+//   return Math.max(min, Math.min(max, val));
+// }
+
+let includeFiles: string[] = ["**/*.js", "**/*.ts"];
+let excludeFiles: string[] = ["!node_modules/**"];
 
 function getFunctionMarksSettings(): FunctionMarksSettings {
   const config = vscode.workspace.getConfiguration('function-marks');
+  includeFiles = config.get<string>(
+      "includeFiles", "**/*.js, **/*.ts").split(",").map(p => p.trim());
+  excludeFiles = config.get<string>(
+      "excludeFiles", "!node_modules/**").split(",").map(p => p.trim());
   return {
     fileWrap:            config.get('fileWrap',            true),
     includeSubFunctions: config.get('includeSubFunctions', false),
@@ -33,3 +39,11 @@ function getFunctionMarksSettings(): FunctionMarksSettings {
 export function refreshSettings() {
   settings = getFunctionMarksSettings();
 }
+
+export function includeDocument(fsPath: string): boolean {
+  const filePath = vscode.workspace.asRelativePath(fsPath);
+  const isInc    = includeFiles.some(pattern => minimatch(filePath, pattern));
+  const isExc    = excludeFiles.some(pattern => minimatch(filePath, pattern));
+  return isInc && !isExc;
+}
+
