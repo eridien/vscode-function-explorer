3013284b2d01035ca97fe96348034abd46df7044
Mark Hahn <mark@hahnca.com>
2025-07-13 21:44:59 -0700

    added c lang

--- src/languages.ts
+++ src/languages.ts
@@ -1,42 +1,43 @@
 
 export const langs: any = {
 
+///////////////////////////// typescript ///////////////////////////
   typescript: {
     sExpr: `
       [
         ((function_declaration
-            name: (identifier) @funcDeclName) @funcDecl)
+            name: (identifier) @ts.funcDeclName) @ts.funcDecl)
         ((variable_declarator
-            name: (identifier) @funcExprDeclName
-            value: (function_expression) @funcExprDecl) @funcExprDeclBody)
+            name: (identifier) @ts.funcExprDeclName
+            value: (function_expression) @ts.funcExprDecl) @ts.funcExprDeclBody)
         ((variable_declarator
-            name: (identifier) @arrowFuncDeclName
-            value: (arrow_function) @arrowFuncDecl) @arrowFuncDeclBody)
+            name: (identifier) @ts.arrowFuncDeclName
+            value: (arrow_function) @ts.arrowFuncDecl) @ts.arrowFuncDeclBody)
         ((class_declaration
-            name: (type_identifier) @classDeclName) @classDecl)
+            name: (type_identifier) @ts.classDeclName) @ts.classDecl)
         ((method_definition
-            name: (property_identifier) @methodDefName) @methodDef)
+            name: (property_identifier) @ts.methodDefName) @ts.methodDef)
         ((pair
-            key: (property_identifier) @propertyName) @property) @propertyBody
+            key: (property_identifier) @ts.propertyName) @ts.property) @ts.propertyBody
         ((assignment_expression
             left: [(identifier) (member_expression) (subscript_expression)]
-                                                      @assExprName) @assExpr)
+                                                      @ts.assExprName) @ts.assExpr)
         ((variable_declarator
-            name: (identifier) @varDeclName) @varDecl)
+            name: (identifier) @ts.varDeclName) @ts.varDecl)
          ((namespace_import
-            (identifier) @importName) @import)
+            (identifier) @ts.importName) @ts.import)
      ]
     `,
     capTypes: new Map<string, string>([
-      ['funcDecl',          'function_declaration'],
-      ['funcExprDeclBody',  'function_expression'],
-      ['arrowFuncDeclBody', 'arrow_function'],
-      ['classDecl',         'class_declaration'],
-      ['methodDef',         'method_definition'],
-      ['propertyBody',      'pair'],
-      ['assExpr',           'assignment_expression'],
-      ['varDecl',           'variable_declarator'],
-      ['import',            'namespace_import'],
+      ['ts.funcDecl',          'function_declaration'],
+      ['ts.funcExprDeclBody',  'function_expression'],
+      ['ts.arrowFuncDeclBody', 'arrow_function'],
+      ['ts.classDecl',         'class_declaration'],
+      ['ts.methodDef',         'method_definition'],
+      ['ts.propertyBody',      'pair'],
+      ['ts.assExpr',           'assignment_expression'],
+      ['ts.varDecl',           'variable_declarator'],
+      ['ts.import',            'namespace_import'],
     ]),
     symbols: new Map<string, string>([
       ['function_declaration',  'ƒ'],
@@ -58,25 +59,26 @@ export const langs: any = {
     suffixes:    new Set(['.js', '.ts', '.tsx', '.jsx'])
   },
 
+///////////////////////////// python ///////////////////////////
   python: {
     sExpr: `
       [
         ((function_definition
-          name: (identifier) @funcDefName) @funcDef)
+          name: (identifier) @py.funcDefName) @py.funcDef)
         ((class_definition
-          name: (identifier) @classDefName) @classDef)
+          name: (identifier) @py.classDefName) @py.classDef)
         ((assignment
-          (pattern) @namedExprName) @namedExpr)
+          (pattern) @py.namedExprName) @py.namedExpr)
         ((import_from_statement
-          module_name: (dotted_name) @importFromName) @importFrom)
+          module_name: (dotted_name) @py.importFromName) @py.importFrom)
       ]
     `,
     capTypes: new Map<string, string>([
-      ['funcDef',   'function_definition'],
-      ['classDef',  'class_definition'],
-      ['namedExpr', 'assignment'],
-      ['namedExpr', 'assignment'],
-      ['importFrom','import_from_statement'],
+      ['py.funcDef',   'function_definition'],
+      ['py.classDef',  'class_definition'],
+      ['py.namedExpr', 'assignment'],
+      ['py.namedExpr', 'assignment'],
+      ['py.importFrom','import_from_statement'],
     ]),
     symbols: new Map([
       ['function_definition',   'ƒ'],
@@ -89,22 +91,32 @@ export const langs: any = {
     suffixes:    new Set(['.py'])
   },
 
-  // c: {
-  //   sExpr: `
-  //     [
-  //       ((function_definition
-  //         body: (compound_statement) @funcDefName) @funcDef)
-  //     ]
-  //   `,
-  //   capTypes: new Map<string, string>([
-  //     ['funcDef',   'function_definition'],
-  //   ]),
-  //   symbols: new Map([
-  //     ['function_definition',   'ƒ'],
-  //   ]),
-  //   funcTypes:   new Set(["function_definition"]),
-  //   lowPriority: new Set(),
-  //   suffixes:    new Set(['.c'])
-  // },
+///////////////////////////// c ///////////////////////////
+  c: {
+    sExpr: `
+      [
+        ((function_definition
+          declarator: (function_declarator
+            declarator: (identifier) @c.funcDefName)) @c.funcDef)
+        ((assignment_expression
+          left: (identifier) @c.assExprName) @c.assExpr)
+        ((call_expression
+          function: (identifier) @c.callExprName) @c.callExpr)
+      ]
+    `,
+    capTypes: new Map<string, string>([
+      ['c.funcDef',  'function_definition'],
+      ['c.assExpr',  'assignment_expression'],
+      ['c.callExpr', 'call_expression'],
+    ]),
+    symbols: new Map([
+      ['function_definition',   'ƒ'],
+      ['assignment_expression', '='],
+      ['call_expression',       '('],
+    ]),
+    funcTypes:   new Set(["function_definition"]),
+    lowPriority: new Set(),
+    suffixes:    new Set(['.c'])
+  },
 
 };
